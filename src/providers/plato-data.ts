3286a4fdcb0558b426ca711904651d54dbb0024f
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2016-11-30 22:52:24 -0300

    Se agrega proveedor de la informacion de los platos

---
+++ src/providers/plato-data.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the PlatoData provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class PlatoData {
+
+  constructor() {
+    console.log('Hello PlatoData Provider');
+  }
+
+}
