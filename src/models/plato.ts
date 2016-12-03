f5a7643ca24b2f2e0fef130a005c654d48c57196
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2016-12-03 15:23:09 -0300

    Se agrega detalle del plato

--- src/models/plato.ts
+++ src/models/plato.ts
@@ -9,4 +9,26 @@ export class Plato {
             this.nombre.toLowerCase().replace(/\s/g, "-") +
             this.extensionFoto;
     }
+
+    get descripcion(): string {
+        return "El lomo saltado es un plato típico de la gastronomía del Perú cuyos registros datan de fines del siglo XIX, donde se le conocía como «lomito de vaca», «lomito saltado» o «lomito a la chorrillana».1" +
+            "El plato tal como se definió en aquella época y se le conoce actualmente surge por la influencia de los chinos-cantoneses, contiene la sazón y la mezcla de la cocina criolla peruana con la oriental. La influencia oriental se demuestra por el uso de la técnica de cocción en sartén," + "ahora conocida como «lomo saltado».2 Hay variantes introducidas en este plato, porque dependiendo del gusto, se han reemplazado unos ingredientes por otros. Este plato es uno de los más consumidos popularmente en el Perú.3";
+    }
+
+    get ingredientes(): string[] {
+        return [
+            "Carne de res",
+            "sal",
+            "pimienta",
+            "comino",
+            "cebolla",
+            "ajo",
+            "ají verde",
+            "vinagre tinto o blanco",
+            "tomate",
+            "perejil",
+            "papas",
+            "aceite"
+        ]
+    }
 }
