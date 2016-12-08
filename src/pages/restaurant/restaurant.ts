eab696ad4e75a70b8e71f1019deff66200f0e2da
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2016-12-08 20:10:16 -0300

    Se agrega mapa del restaurant

--- src/pages/restaurant/restaurant.ts
+++ src/pages/restaurant/restaurant.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 
 import { NavController } from "ionic-angular";
+import { InAppBrowser } from "ionic-native";
 
 @Component({
     selector: "page-restaurant",
@@ -8,4 +9,10 @@ import { NavController } from "ionic-angular";
 })
 export class RestaurantPage {
     constructor(public navCtrl: NavController) { }
+
+    abrirMapa() {
+        let urlGoogleMaps = "https://www.google.cl/maps/place/Pardo's+Chicken/@-33.4184206,-70.6019252,15.67z/data=!4m5!3m4!1s0x0:0xfd38697b954cb275!8m2!3d-33.417065!4d-70.598408";
+        console.log("Abrir mapa", urlGoogleMaps);
+        new InAppBrowser(urlGoogleMaps, "_system");
+    }
 }
