d5c4e2693ee701f712a40b55e690a2091b01f540
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2016-12-10 20:31:07 -0300

    Se utiliza reserva de asientos basada en codepen

--- src/pages/mesas/mesas.ts
+++ src/pages/mesas/mesas.ts
@@ -3,11 +3,10 @@ import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
 
 @Component({
-    selector: "mesas",
+    selector: "page-mesas",
     templateUrl: "mesas.html"
 })
 export class MesasPage {
-    // TODO: Considerar agregar la opcion para filtrar mesas para fumadores y otras cosas
     fechaReserva: string;
     filtrosMesas: string[];
 
