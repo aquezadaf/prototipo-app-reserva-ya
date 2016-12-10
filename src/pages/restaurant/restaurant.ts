df7c920d150b2586fa77fcf6a67313fdb3463aa8
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2016-12-09 21:58:30 -0300

    Se agregan links de contactos

--- src/pages/restaurant/restaurant.ts
+++ src/pages/restaurant/restaurant.ts
@@ -8,11 +8,36 @@ import { InAppBrowser } from "ionic-native";
     templateUrl: "restaurant.html"
 })
 export class RestaurantPage {
+    contactos = {
+        direccion: "Av. Concha Y Toro 1149, Puente Alto",
+        twitter: "reservayachile",
+        correo: "reserva.ya.chile@gmail.com",
+        telefono: "+569 1234 5678"
+    };
+
     constructor(public navCtrl: NavController) { }
 
     abrirMapa() {
         let urlGoogleMaps = "https://www.google.cl/maps/place/Pardo's+Chicken/@-33.4184206,-70.6019252,15.67z/data=!4m5!3m4!1s0x0:0xfd38697b954cb275!8m2!3d-33.417065!4d-70.598408";
-        console.log("Abrir mapa", urlGoogleMaps);
-        new InAppBrowser(urlGoogleMaps, "_system");
+        this.abrirNavegador(urlGoogleMaps);
+    }
+
+    abrirTwitter() {
+        let urlPerfilTwitter = "https://twitter.com/" + this.contactos.twitter;
+        this.abrirNavegador(urlPerfilTwitter);
+    }
+
+    abrirCorreo() {
+        let urlCorreo = "mailto:" + this.contactos.correo;
+        this.abrirNavegador(urlCorreo);
+    }
+
+    abrirLlamada() {
+        let urlLlamada = "tel:" + this.contactos.telefono;
+        this.abrirNavegador(urlLlamada);
+    }
+
+    private abrirNavegador(urlSitio: string) {
+        new InAppBrowser(urlSitio, "_system");
     }
 }
