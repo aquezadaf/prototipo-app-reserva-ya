4d4302f1b978698c3a5dffedc53777c2beb58f3d
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2016-12-12 00:07:37 -0300

    Se genera interfaz de la agendacion de reserva

--- src/pages/agendar-reserva/agendar-reserva.ts
+++ src/pages/agendar-reserva/agendar-reserva.ts
@@ -9,15 +9,21 @@ import { Mesa } from "../../models/mesa";
 export class AgendarReservaPage {
     public mesa: Mesa;
     public fechaReserva: string;
+    public horaReserva: string;
     public estaEnExterior: boolean;
 
     constructor(public viewCtrl: ViewController, private navParams: NavParams) { 
         this.mesa = navParams.get("mesa");
-        this.fechaReserva = navParams.get("fechaReserva");
+        this.fechaReserva = navParams.get("fechaReserva").substring(0, 10);
+        this.horaReserva = navParams.get("fechaReserva").substring(11, 19);
         this.estaEnExterior = navParams.get("mesaEnExterior");
     }
 
     cerrarModal() {
         this.viewCtrl.dismiss();
+    }
+
+    reservar() {
+        this.cerrarModal();
     }
 }
