89f8cee4b34856d4e1395ab354e20fc6cd5cc3ea
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2016-12-11 21:00:09 -0300

    Se agrega modulo agendar reserva

---
+++ src/pages/agendar-reserva/agendar-reserva.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+
+@Component({
+    selector: "page-agendar-reserva",
+    templateUrl: "agendar-reserva.html"
+})
+export class AgendarReservaPage {
+    constructor(public navController: NavController) { }
+}
